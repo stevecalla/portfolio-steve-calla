ec77ea1cddc00b41aebcd08e3404540b83eddf07
Steve Calla <callasteven@gmail.com>
2022-06-26 14:07:19 -0600

    Update google font and font weight

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 //query selector variables go here 👇
+
 let hamburgerMenu = document.getElementById("hamburger-menu");
 let navMenu = document.getElementById("nav-menu");
 
 //global variables go here 👇
+
 let intervalId = null;
 
 //event listeners go here 👇
+
 hamburgerMenu.addEventListener("click", displayMenu);
 window.addEventListener("resize", screenWidthDisplay);
 
 //functions and event handlers go here 👇
+
 function displayMenu() {
   if (window.getComputedStyle(navMenu).display === "none") {
     navMenu.style.display = "flex";
@@ -17,7 +21,6 @@ function displayMenu() {
     intervalId = setTimeout(() => {
       navMenu.style.display = "none";
       hamburgerMenu.style.color = "black";
-      console.log(intervalId);
     }, 10000);
   } else {
     navMenu.style.display = "none";
@@ -27,6 +30,7 @@ function displayMenu() {
 }
 
 // displayMenu function side effect = header nav menu will not display if screen width is changed durin the timeout; the function below ensures header nav menu displays >768 & clears the timeout from displayMenu if there is an overlap
+
 function screenWidthDisplay() {
   if (window.innerWidth > 768) {
     clearTimeout(intervalId);
