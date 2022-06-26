9fa2e9b9bc1a916e1b8f86d1c7f2d1a0ba0a9dbb
Steve Calla <callasteven@gmail.com>
2022-06-26 12:41:37 -0600

    Significant refactor

--- app.js
+++ app.js
@@ -3,20 +3,37 @@ let hamburgerMenu = document.getElementById("hamburger-menu");
 let navMenu = document.getElementById("nav-menu");
 
 //global variables go here 👇
+let intervalId = null;
 
 //event listeners go here 👇
 hamburgerMenu.addEventListener("click", displayMenu);
+window.addEventListener("resize", screenWidthDisplay);
+
 
 //functions and event handlers go here 👇
 function displayMenu() {
   if (window.getComputedStyle(navMenu).display === "none") {
     navMenu.style.display = "flex";
     hamburgerMenu.style.color = "cornflowerblue";
-    setInterval(() => {
+    intervalId = setTimeout(() => {
       navMenu.style.display = "none";
       hamburgerMenu.style.color = "black";
+      console.log(intervalId);
     }, 10000);
   } else {
+    navMenu.style.display = "none";
+    hamburgerMenu.style.color = "black";
+    clearTimeout(intervalId);
+  }
+}
+
+// displayMenu function side effect = header nav menu will not display if screen width is changed durin the timeout; the function below ensures header nav menu displays >768 & clears the timeout from displayMenu if there is an overlap 
+function screenWidthDisplay() {
+  if (window.innerWidth > 768) {
+    clearTimeout(intervalId);
+    navMenu.style.display = "flex";
+  } else if (window.innerWidth <= 768) {
+    clearTimeout(intervalId);
     navMenu.style.display = "none";
     hamburgerMenu.style.color = "black";
   }
